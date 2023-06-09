2af42adefa58fcd2a1e79f33e398ac3d03ade623
RegistryROK <registry.forbusiness@gmail.com>
2023-06-09 12:58:27 +0900

    add: main.ts, apply pipe globally

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+import AppModule from '@/modules/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const logger = new Logger('bootstrap');
+  const port = process.env.PORT || 4000;
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await app.listen(port, () => {
+    logger.log(`Server Listening on port ${port}`);
+  });
+}
+bootstrap();
