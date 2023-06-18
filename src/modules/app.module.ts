ab34a43a8de4ae3eec28f681af7c3d9195f03e1a
RegistryROK <registry.forbusiness@gmail.com>
2023-06-19 00:40:11 +0900

    add: ConfigModule and TypeOrmModule

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,8 +1,13 @@
 import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import envConfig from '@/configs/env.config';
+import ormConfig from '@/configs/orm.config';
 import LoggerMiddleware from '@/middlewares/logger.middleware';
 
 @Module({
+  imports: [ConfigModule.forRoot(envConfig()), TypeOrmModule.forRoot(ormConfig())],
   providers: [Logger],
 })
 export default class AppModule implements NestModule {
